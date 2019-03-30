3e81e9dd9cc6472125bf0ab08f19dfa8f5f6d19d
RichardF12 <37163455+richardf12@users.noreply.github.com>
2019-03-29 19:50:14 -0600

    Layout 2 done

--- app/src/main/java/com/example/evaluacionpdm1/NewActivity.java
+++ app/src/main/java/com/example/evaluacionpdm1/NewActivity.java
@@ -1,4 +1,16 @@
 package com.example.evaluacionpdm1;
 
+import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
 public class NewActivity {
+    private TextView user, mail, total, tv_1, tv_2, tv_3, tv_4, tv_5, tv_6, tv_7, tv_8, tv_9;
+    private LinearLayout prod1, prod2, prod3, prod4, prod5, prod6, prod7, prod8, prod9;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new);
+    }
 }
