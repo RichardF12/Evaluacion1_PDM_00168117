1c2e55674c26d78fe16025c2ea7a6c96c895b553
RichardF12 <37163455+richardf12@users.noreply.github.com>
2019-03-29 19:55:03 -0600

    :c

--- app/src/main/java/com/example/evaluacionpdm1/NewActivity.java
+++ app/src/main/java/com/example/evaluacionpdm1/NewActivity.java
@@ -1,10 +1,11 @@
 package com.example.evaluacionpdm1;
 
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-public class NewActivity {
+public class NewActivity extends AppCompatActivity {
     private TextView user, mail, total, tv_1, tv_2, tv_3, tv_4, tv_5, tv_6, tv_7, tv_8, tv_9;
     private LinearLayout prod1, prod2, prod3, prod4, prod5, prod6, prod7, prod8, prod9;
 
